9ced3933a0ccf56222363a8da4ee13db79b08c96
Fizer Khan <fizerkhan@gmail.com>
2015-10-07 20:22:53 +0530

    Fixed suffix.

--- jobs/usagewatch.rb
+++ jobs/usagewatch.rb
@@ -4,11 +4,11 @@ SCHEDULER.every '5m', :first_in => 0 do
 
   usw = Usagewatch
   send_event("usagewatch", {
-    diskused: usw.uw_diskused,
-    diskused_perc: usw.uw_diskused_perc,
-    memused: usw.uw_memused,
-    cpuused: usw.uw_cpuused,
-    tcpused: usw.uw_tcpused
+    diskused: usw.uw_diskused.to_s + ' GB',
+    diskused_perc: usw.uw_diskused_perc.to_s + ' %',
+    memused: usw.uw_memused.to_s + ' %',
+    cpuused: usw.uw_cpuused.to_s + ' %',
+    tcpused: usw.uw_tcpused.to_s
   })
 
 end
